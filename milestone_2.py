a9d45697e3c1ea6ad479cb953a20126196812c07
Yurishizu <67028414+Yurishizu9@users.noreply.github.com>
2022-11-22 17:19:33 +0000

    checks that the input is a single character

--- milestone_2.py
+++ milestone_2.py
@@ -8,4 +8,8 @@ print(word)
 #%% ask the user for the input
 guess = input('enter a single letter: ')
 
+if guess.isalpha() and len(guess) == 1:
+    print('Good guess!')
+else:
+    print('Oops! That is not a valid input.')
 # %%
