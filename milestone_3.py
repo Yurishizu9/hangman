a8d38b69b4ae2a7d0ef9004ee2767b9a8d4a5a76
Yurishizu <67028414+Yurishizu9@users.noreply.github.com>
2022-11-22 19:34:59 +0000

    chooses a random word from a list

--- milestone_3.py
+++ milestone_3.py
@@ -1,3 +1,9 @@
+#%% choose a random word from list
+import random
+
+word_list = ['mango', 'banana', 'pear', 'apple', 'coconut']
+word = random.choice(word_list)
+
 #%% checks if input is vallid
 while True:
     guess = input('Please, enter a single alphabetical character: ')
