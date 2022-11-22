2692a7b94d66f72a7051b061b8675dfa60d88301
Yurishizu <67028414+Yurishizu9@users.noreply.github.com>
2022-11-22 17:16:24 +0000

    ask the user for the input

--- milestone_2.py
+++ milestone_2.py
@@ -1,6 +1,11 @@
+#%% choose a random word from list
 import random
 
 word_list = ['mango', 'banana', 'pear', 'apple', 'coconut']
 word = random.choice(word_list)
-
 print(word)
+
+#%% ask the user for the input
+guess = input('enter a single letter: ')
+
+# %%
