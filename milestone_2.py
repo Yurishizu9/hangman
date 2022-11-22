de3cb7d0fce93e7350759cde6058b920e74ebfab
Yurishizu <67028414+Yurishizu9@users.noreply.github.com>
2022-11-22 19:26:52 +0000

    devides the code into their appropiate blocks

--- milestone_2.py
+++ milestone_2.py
@@ -1,15 +1,18 @@
+#%% defined a list of possible words
+word_list = ['mango', 'banana', 'pear', 'apple', 'coconut']
+
 #%% choose a random word from list
 import random
 
-word_list = ['mango', 'banana', 'pear', 'apple', 'coconut']
 word = random.choice(word_list)
 print(word)
 
 #%% ask the user for the input
 guess = input('enter a single letter: ')
 
+
+# %% check that the input is a single character
 if guess.isalpha() and len(guess) == 1:
     print('Good guess!')
 else:
     print('Oops! That is not a valid input.')
-# %%
